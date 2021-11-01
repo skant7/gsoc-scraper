d01f64d3042acf89f6964aaf691adebf59cec49f
Surya Kant <suryakantasahoo533@gmail.com>
2021-11-01 18:49:42 +0530

    Added a simple CLI tool to search for orgs

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # gsoc-scraper
 A scraper to filter organizations based on tech being used
+
+## How to Install on local:
+
+- Requirements
+    - Python 3.8+
+## Run this command:
+    > `pip install requests beautifulsoup4`
+
+    > `python3 script.py java python....`
+    
+
+#### List that you get on the terminal is organizations use the tech you passed as arguments
+
+
---
+++ script.py
@@ -0,0 +1,31 @@
+import requests
+import argparse
+from bs4 import BeautifulSoup
+
+
+parser = argparse.ArgumentParser(description="Technologies to be searched for")
+parser.add_argument('tech' ,metavar='javscript' ,type=str,nargs='+',help='tech to be searched')
+args = parser.parse_args()
+
+
+to_visit = set()
+response = requests.get('https://summerofcode.withgoogle.com/archive/2021/organizations')
+soup = BeautifulSoup(response.content, "html.parser")
+
+for a in soup.select('a.organization-card__link'):
+    to_visit.add(a.get('href'))
+
+base_url = 'https://summerofcode.withgoogle.com'
+print(to_visit)
+result_orgs = set()
+for org_link in to_visit:
+    response = requests.get(base_url+org_link)
+    #print(base_url+org_link)
+    print(base_url+org_link)
+    soup = BeautifulSoup(response.content, "html.parser")
+    for tech_used in (soup.select('li.organization__tag--technology')):
+        if tech_used.text in args.tech:
+            result_orgs.add((soup.find('title').text))
+
+
+print(result_orgs)
